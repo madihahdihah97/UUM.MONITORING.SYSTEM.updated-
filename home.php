02149acd464ec4063a30ceb21da3c62c62807eac
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-04 19:25:09 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,30 @@
+<?php
+    
+session_start();
+if(!isset($_SESSION['username'])){
+    header('location:login.php');
+}
+?>
+
+
+<html>
+<head>
+   <title> Homepage </title> 
+    <link rel="stylesheet" type="text/css" href="style.css">
+      <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+    </head>
+<body>
+    
+    <div class="container">
+        
+    <a class="float-right" href="logout.php"> LOGOUT </a>
+        
+    <a href="logout.php"> LOGOUT </a>
+    
+    <h1>Welcome <?php echo $_SESSION['username'];?> </h1>
+    
+        </div>
+    </body>
+
+</html>
